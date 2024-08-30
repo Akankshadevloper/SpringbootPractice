34c6a547c2df16571e6dccb3acff690ea223639a
Akanksha Kumari <200301120008@cutm.ac.in>
2024-08-30 14:24:48 +0530

    customer form

--- validationdemo/src/main/java/com/akanksha/springdemo/mvc/Customer.java
+++ validationdemo/src/main/java/com/akanksha/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.akanksha.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -10,6 +12,21 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1,message = "is required")
     private String lastName = " ";
+
+
+    @Min(value = 0, message = "must be grater than or equal to zero")
+    @Max(value = 10, message = "must be grater than or equal to 10")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+
 
 
     public String getFirstName() {
