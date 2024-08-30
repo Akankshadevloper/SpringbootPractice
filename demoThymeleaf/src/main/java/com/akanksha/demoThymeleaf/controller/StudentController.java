c15fc663d2cee8919ff9f0a6e62af7287931adb4
Akanksha Kumari <200301120008@cutm.ac.in>
2024-08-30 11:19:54 +0530

    studentForm

--- demoThymeleaf/src/main/java/com/akanksha/demoThymeleaf/controller/StudentController.java
+++ demoThymeleaf/src/main/java/com/akanksha/demoThymeleaf/controller/StudentController.java
@@ -2,14 +2,26 @@ package com.akanksha.demoThymeleaf.controller;
 
 
 import com.akanksha.demoThymeleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${systems}")
+    private List<String> systems;
+
+
+
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -22,6 +34,12 @@ public class StudentController {
 
         //add student object to the model
         theModel.addAttribute("student", theStudent);
+
+        //add the list of language to the model
+        theModel.addAttribute("countries", countries);
+
+        //add the list of systems to the model
+        theModel.addAttribute("systems", systems);
 
 
         return "student-form";
--- demoThymeleaf/src/main/java/com/akanksha/demoThymeleaf/model/Student.java
+++ demoThymeleaf/src/main/java/com/akanksha/demoThymeleaf/model/Student.java
@@ -1,10 +1,19 @@
 package com.akanksha.demoThymeleaf.model;
 
+import java.util.List;
+
 public class Student {
 
     private  String firstName;
 
     private String lastName;
+
+    private String country;
+
+
+    private String favoriteLanguage;
+
+    private List<String> favoriteSystems;
 
     public  Student() {
 
@@ -24,5 +33,31 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+
+    public List<String> getFavoriteSystem() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystem(List<String> favoriteSystem) {
+        this.favoriteSystems = favoriteSystem;
     }
 }
