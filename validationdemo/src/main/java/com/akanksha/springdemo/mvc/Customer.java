f9f9ea701a01d56c8d245666f203533aac14aee3
Akanksha Kumari <200301120008@cutm.ac.in>
2024-08-30 22:18:38 +0530

    table creation

--- validationdemo/src/main/java/com/akanksha/springdemo/mvc/Customer.java
+++ validationdemo/src/main/java/com/akanksha/springdemo/mvc/Customer.java
@@ -1,5 +1,6 @@
 package com.akanksha.springdemo.mvc;
 
+import com.akanksha.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -18,6 +19,17 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}" ,message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value = "TOPS", message = "must start with TOPS")
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getPostalCode() {
         return postalCode;
---
+++ validationdemo/src/main/java/com/akanksha/springdemo/mvc/validation/CourseCode.java
@@ -0,0 +1,54 @@
+package com.akanksha.springdemo.mvc.validation;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+
+
+
+    //define default course code
+
+    public String value() default "AKA";
+
+
+    // define default error message
+    public String message() default "must start with AKA";
+
+
+    //define default groups
+    public Class<?>[] groups() default {};
+
+
+    //define default payload
+    public Class<? extends Payload>[] payload() default {};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
