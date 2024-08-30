a47fc77ce686ab5d177c63a9fd1242e2ea3dcaa6
Akanksha Kumari <200301120008@cutm.ac.in>
2024-08-30 14:12:01 +0530

    validation

---
+++ validationdemo/src/main/java/com/akanksha/springdemo/mvc/ValidationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.akanksha.springdemo.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationdemoApplication.class, args);
+	}
+
+}
---
+++ validationdemo/src/test/java/com/akanksha/springdemo/mvc/ValidationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akanksha.springdemo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
