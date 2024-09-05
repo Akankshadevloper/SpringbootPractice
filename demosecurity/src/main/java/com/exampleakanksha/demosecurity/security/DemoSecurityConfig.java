f633c4750e9451d75d83b55f8d92537b885c4fb4
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-05 09:45:47 +0530

    spring security database

--- demosecurity/src/main/java/com/exampleakanksha/demosecurity/security/DemoSecurityConfig.java
+++ demosecurity/src/main/java/com/exampleakanksha/demosecurity/security/DemoSecurityConfig.java
@@ -3,14 +3,59 @@ package com.exampleakanksha.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // Define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id as username, pw as password, active as enabled from members where user_id = ?"
+        );
+
+        // Define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id as username, role from roles where user_id = ?"
+        );
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/showMyLoginPage")  // Custom login page URL
+                                .loginProcessingUrl("/authenticateTheUser")  // URL to submit the username and password
+                                .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
+                )
+                .exceptionHandling(configurer ->
+                        configurer.accessDeniedPage("/access-denied")
+                );
+
+        return http.build();
+    }
+}
+
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
@@ -33,28 +78,5 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
+*/
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/").hasRole("EMPLOYEE")
-                                .requestMatchers("/leaders/**").hasRole("MANAGER")
-                                .requestMatchers("/systems/**").hasRole("ADMIN")  // Correct URL to match your mapping and Thymeleaf link
-                                .anyRequest().authenticated()
-                )
-                .formLogin(formLogin ->
-                        formLogin
-                                .loginPage("/showMyLoginPage")  // Custom login page URL
-                                .loginProcessingUrl("/authenticateTheUser")  // URL to submit the username and password
-                                .permitAll()
-                )
-                .logout(logout -> logout.permitAll()
-                )
-                .exceptionHandling(configurer ->
-                        configurer.accessDeniedPage( "/access-denied")
-                );                        ;
-
-        return http.build();
-    }
-}
