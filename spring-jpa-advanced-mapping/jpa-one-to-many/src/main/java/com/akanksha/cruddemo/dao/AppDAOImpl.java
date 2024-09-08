4846ff3a86fe81eeeeee34b62d288c7dc387f4f5
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-08 09:28:58 +0530

    minimize the code

--- spring-jpa-advanced-mapping/jpa-one-to-many/src/main/java/com/akanksha/cruddemo/dao/AppDAOImpl.java
+++ spring-jpa-advanced-mapping/jpa-one-to-many/src/main/java/com/akanksha/cruddemo/dao/AppDAOImpl.java
@@ -93,6 +93,7 @@ public class AppDAOImpl implements AppDAO{
         TypedQuery<Instructor> query = entityManager.createQuery(
                 "select i from Instructor i "
                 + "JOIN FETCH i.courses "
+                + "JOIN FETCH i.instructorDetail "
                 + "where i.id = :data", Instructor.class);
 
         query.setParameter("data", theId);
