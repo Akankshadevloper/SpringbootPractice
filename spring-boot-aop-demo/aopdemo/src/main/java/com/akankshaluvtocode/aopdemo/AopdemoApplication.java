48b00ba6aaa79945948c541b037838f0effb138b
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-10 08:26:02 +0530

    aspect oriented programming

---
+++ spring-boot-aop-demo/aopdemo/src/main/java/com/akankshaluvtocode/aopdemo/AopdemoApplication.java
@@ -0,0 +1,37 @@
+package com.akankshaluvtocode.aopdemo;
+
+import com.akankshaluvtocode.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+
+		return runner -> {
+
+			demoTheBeforeAdvice(theAccountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+
+		//call the business method
+		theAccountDAO.addAccount();
+	}
+}
+
+
+
+
+
+
+
---
+++ spring-boot-aop-demo/aopdemo/src/main/java/com/akankshaluvtocode/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,8 @@
+package com.akankshaluvtocode.aopdemo.dao;
+
+public interface AccountDAO {
+
+
+    void addAccount();
+
+}
