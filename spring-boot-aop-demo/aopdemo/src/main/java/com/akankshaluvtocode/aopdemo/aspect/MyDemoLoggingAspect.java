1dc5edf946c5c5de7a1456a295b2bfec8768141c
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-10 08:53:46 +0530

    only addaccount

--- spring-boot-aop-demo/aopdemo/src/main/java/com/akankshaluvtocode/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-boot-aop-demo/aopdemo/src/main/java/com/akankshaluvtocode/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -14,7 +14,7 @@ public class MyDemoLoggingAspect {
 
     //let's start with an @Before advice
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.akankshaluvtocode.aopdemo.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n=====>>>> Executing @Before advice on addAccount()");
