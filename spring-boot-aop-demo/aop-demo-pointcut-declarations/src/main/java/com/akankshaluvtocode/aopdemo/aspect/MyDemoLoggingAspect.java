1a95f38b28a0a2ca654339e413c66af5c77d9aa8
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-10 10:50:19 +0530

    need to modify app match method in a dao package and exclude  getter/setter method

--- spring-boot-aop-demo/aop-demo-pointcut-declarations/src/main/java/com/akankshaluvtocode/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-boot-aop-demo/aop-demo-pointcut-declarations/src/main/java/com/akankshaluvtocode/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -14,12 +14,24 @@ public class MyDemoLoggingAspect {
     @Pointcut("execution(* com.akankshaluvtocode.aopdemo.dao.*.*(..))")
     private void forDaoPackage(){}
 
-    @Before("forDaoPackage()")
+    //create a pointcut for getter methods
+    @Pointcut("execution(* com.akankshaluvtocode.aopdemo.dao.*.get*(..))")
+    private void getter() {}
+
+    //create a pointcut for setter methods
+    @Pointcut("execution(* com.akankshaluvtocode.aopdemo.dao.*.set*(..))")
+    private void setter() {}
+
+    //create pointcut : include package ... exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {}
+
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>>> Executing @Before advice on method");
     }
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void performApiAnalytics() {
         System.out.println("\n=====>>> Performing api analytics ");
     }
